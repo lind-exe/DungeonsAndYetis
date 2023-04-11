e7ff4b7f3885d42258d383a64aabf32f28f6a8ec
friborg <hannafriborg@gmail.com>
2023-04-11 18:19:39 +0200

    waow rutnät funkar, inga väggar lol

--- script/script.js
+++ script/script.js
@@ -1,15 +1,48 @@
-drawLabyrinth()
 
-function drawLabyrinth() {
+let iPosition
+let jPosition
+
+start()
+
+function start() {
+    iPosition = 9
+    jPosition = 5
+    drawLabyrinth(iPosition, jPosition)
+}
+
+function drawLabyrinth(iPos, jPos) {
     let labyrinth = ""
     let i = 0
     let j = 0
 
-    for(i = 0; i<=10; i++) {
-        labyrinth += "<div> i." + i + "| j."+ j +"</div>"
-        for(j = 0; j<=10; j++) {
-            labyrinth += "<div> i." + i + "| j."+ j +"</div>"
+    for (i = 0; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
+            if (iPos == i && jPos == j) {
+                labyrinth += "<div> X </div>"
+            }
+            else {
+                labyrinth += "<div> i." + i + "| j." + j + "</div>"
+            }
         }
     }
     document.getElementById("frame").innerHTML = labyrinth
+}
+
+function move(value) {
+    if (value == 'North' && iPosition > 0) {
+        iPosition--;
+        drawLabyrinth(iPosition, jPosition)
+    }
+    if (value == 'West' && jPosition > 0) {
+        jPosition--;
+        drawLabyrinth(iPosition, jPosition)
+    }
+    if (value == 'East' && jPosition < 9) {
+        jPosition++;
+        drawLabyrinth(iPosition, jPosition)
+    }
+    if (value == 'South' && iPosition < 9) {
+        iPosition++;
+        drawLabyrinth(iPosition, jPosition)
+    }
 }
