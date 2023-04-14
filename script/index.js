e0ef2d00309fdfd9b394a16eed36a80d8d4e45d3
friborg <hannafriborg@gmail.com>
2023-04-14 16:30:05 +0200

    not workingh api

--- script/index.js
+++ script/index.js
@@ -1,11 +1,22 @@
-let joke = ""
+let joke
+
+getJoke()
 
 function getJoke() {
-    const url = "https://api.api-ninjas.com/v1/jokes"
+    const url = "https://api.api-ninjas.com/v1/jokes/"
+    const jokeDiv = document.getElementById("jokeDiv")
 
     fetch(url)
-        .then(function (response) { return response.json })
-        .then(function(data){
-            joke
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+            joke = data
+
+            joke.map(function (joke) {
+                let jokeString = document.createElement("h1")
+                jokeString.innerHTML = joke
+
+                jokeDiv.appendChild(jokeString)
+
+            })
         })
 }
